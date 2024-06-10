dbb8b8774a020d840ba3676ddeaa730e9ae91aaa
Mitchell Dederer <mitchelldederer@gmail.com>
2024-06-10 15:58:40 +1000

    feat: progress

--- force-app/main/default/lwc/testContactComponent/testContactComponent.js
+++ force-app/main/default/lwc/testContactComponent/testContactComponent.js
@@ -1,30 +1,47 @@
-import { LightningElement, wire } from 'lwc';
-import getCourses from '@salesforce/apex/CourseController.getCourses'
+import { LightningElement, wire } from "lwc";
+import getCourses from "@salesforce/apex/CourseController.getCourses";
 
 export default class TestContactComponent extends LightningElement {
-    name = "Test Name";
-    department = "Test Department";
+  name = "Test Name";
+  department = "Test Department";
+  alert = "";
 
-    data;
-    error;
+  data;
+  error;
 
-    @wire(getCourses)
-    courses;
+  @wire(getCourses)
+  courses;
 
-    handleClick() {
-        alert('Name: ' + this.name + ' Department: ' + this.department);
-    }
+  handleClick() {
+    this.alert = `Name: ${this.name}-Department: ${this.department}`;
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    setTimeout(() => {
+      this.alert = "";
+    }, 3000);
+  }
 
-    handleClear() {
-        this.name = '';
-        this.department = '';
-    }
+  handleClear() {
+    this.name = "";
+    this.department = "";
+  }
 
-    handleNameChange(e) {
-        this.name = e.target.value; 
-    }
+  handleNameChange(e) {
+    this.name = e.target.value;
+  }
 
-    handleDepartmentChange(e) {
-        this.department = e.target.value;
-    }
+  handleDepartmentChange(e) {
+    this.department = e.target.value;
+  }
+
+  get isAlertFilled() {
+    return this.alert !== "";
+  }
+
+  get firstAlert() {
+    return this.alert.split("-")[0];
+  }
+
+  get secondAlert() {
+    return this.alert.split("-")[1];
+  }
 }
